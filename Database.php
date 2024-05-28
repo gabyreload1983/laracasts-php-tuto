a3f62c728d4c9900410cf11813aed9cca67b804c
Gabriel Godoy <gabyreload@gmail.com>
2024-05-28 19:40:14 -0300

    config file

--- Database.php
+++ Database.php
@@ -4,12 +4,13 @@ class Database
 {
     public $connection;
 
-    public function __construct()
-    {
-
-        $dsn = "mysql:host=127.0.0.1;port=3306;dbname=php-tuto;user=root;password=test1234;charset=utf8mb4";
-
-        $this->connection = new PDO($dsn);
+    public function __construct($config, $username = "root", $password = "test1234")
+    {      
+        $dsn = "mysql:" . http_build_query($config,"",";");
+        
+        $this->connection = new PDO($dsn, $username, $password,[
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]);
     }
 
     public function query($query)
--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
 
 require "functions.php"; 
+// require "router.php"; 
 require "Database.php"; 
 
-// require "router.php"; 
+$config = require "config.php"; 
 
-
-$db = new Database();
+$db = new Database($config["database"]);
 
 $posts = $db->query("SELECT * FROM posts")->fetchAll(PDO::FETCH_ASSOC);
 $post = $db->query("SELECT * FROM posts LIMIT 1")->fetch(PDO::FETCH_ASSOC);
