290c73221761e3a637b527ccef2ff9c01d0e22e7
Gabriel Godoy <gabyreload@gmail.com>
2024-05-28 19:15:57 -0300

    Database

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <?php
 
 require "functions.php"; 
+require "Database.php"; 
 
-require "router.php"; 
+// require "router.php"; 
+
+
+$db = new Database();
+
+$posts = $db->query("SELECT * FROM posts")->fetchAll(PDO::FETCH_ASSOC);
+$post = $db->query("SELECT * FROM posts LIMIT 1")->fetch(PDO::FETCH_ASSOC);
+
+
+
+foreach($posts as $post){
+    echo "<li>" . $post["title"] . "</li>";
+}
+
+echo "<p>" . $post["title"] . "<p>";
