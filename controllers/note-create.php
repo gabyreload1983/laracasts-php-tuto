d751761eeed7dc14a13ce0c292cdc5934895901b
Gabriel Godoy <gabyreload@gmail.com>
2024-05-29 16:39:44 -0300

    form validation

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -7,10 +7,22 @@ $config = require "config.php";
 $db = new Database($config["database"]);
 
 if($_SERVER["REQUEST_METHOD"] === "POST"){
-   $db->query("INSERT INTO notes(body, user_id) VALUES (:body, :user_id);",[
-    "body" => $_POST["body"],
-    "user_id" => $currentUser
-   ]);
+   $errors = [];
+
+   if(strlen($_POST["body"]) === 0){
+      $errors["body"] = "A body is required";
+   }
+ 
+   if(strlen($_POST["body"]) > 1000){
+      $errors["body"] = "A body can not be more than 1000 characters";
+   }
+
+   if(empty($errors)){
+      $db->query("INSERT INTO notes(body, user_id) VALUES (:body, :user_id);",[
+         "body" => $_POST["body"],
+         "user_id" => $currentUser
+      ]);
+   }
 }
 
 require "views/note-create.view.php"; 
--- views/note-create.view.php
+++ views/note-create.view.php
@@ -7,19 +7,17 @@
         <form method="POST">
             <div class="space-y-12">
                 <div class="pb-3">
-
                     <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-
-
                         <div class="col-span-full">
                             <label for="body" class="block text-sm font-medium leading-6 text-gray-900">Note</label>
                             <div class="mt-2">
                                 <textarea id="body" name="body" rows="3"
-                                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
+                                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"><?= $_POST["body"] ?? "" ?></textarea>
                             </div>
+                            <?php if(isset($errors["body"])) :  ?>
+                            <p class="text-red-400 mt-2"><?= $errors["body"] ?></p>
+                            <?php endif  ?>
                         </div>
-
-
                     </div>
                 </div>
 
