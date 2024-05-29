ee03693a53f6fdad9a22193c875617f63db8e0a9
Gabriel Godoy <gabyreload@gmail.com>
2024-05-29 11:20:10 -0300

    refactor

--- controllers/note.php
+++ controllers/note.php
@@ -9,16 +9,12 @@ $db = new Database($config["database"]);
 
 $note = $db->query("SELECT * FROM notes WHERE id = :id",[
     "id" => $_GET["id"]
-])->fetch();
+])->findOrFail();
 
-if(!$note){
-    abort();
-}
+
 
 $currentUserId = 1;
 
-if($note["user_id"] != $currentUserId){
-    abort(Response::FORBIDDEN);
-}
+authorize($note["user_id"] === $currentUserId);
 
 require "views/note.view.php"; 
--- controllers/notes.php
+++ controllers/notes.php
@@ -7,6 +7,6 @@ $db = new Database($config["database"]);
 
 $query = "SELECT * FROM notes WHERE user_id = ?";
 
-$notes = $db->query($query,[1])->fetchAll();
+$notes = $db->query($query,[1])->get();
 
 require "views/notes.view.php"; 
