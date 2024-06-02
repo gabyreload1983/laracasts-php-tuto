f4b618ba5eb3a755250499989694e128ccb473e1
Gabriel Godoy <gabyreload@gmail.com>
2024-06-01 21:09:48 -0300

    login

---
+++ controllers/sessions/store.php
@@ -0,0 +1,45 @@
+<?php
+
+use Core\App;
+use Core\Database;
+use Core\Validator;
+
+$email = $_POST["email"];
+$password = $_POST["password"];
+$errors = [];
+
+$db = App::resolve(Database::class);
+
+if(!Validator::email($email)){
+    $errors["email"] = "Please provide a valid email address";
+}
+
+if(!Validator::string($password)){
+    $errors["password"] = "Please enter a valid password.";
+}
+
+if(!empty($errors)){
+    return view("sessions/create.view.php",[
+        "errors" => $errors,
+    ]);
+}
+
+$user = $db->query("SELECT * FROM users WHERE email = :email",[
+    "email" => $email
+])->find();
+
+if($user && password_verify($password,$user["password"])){
+    login([
+        "email" => $email
+    ]);
+    
+    header("location: /");
+    exit();
+}
+
+
+return view("sessions/create.view.php",[
+    "errors" => [
+        "password" => "Email or password is wrong"
+    ],
+]);
--- views/partials/nav.php
+++ views/partials/nav.php
@@ -74,7 +74,11 @@
                                 alt="">
 
                             <?php else : ?>
-                            <a class="text-white" href="/register">Register</a>
+                            <a href="/register"
+                                class="<?= urlIs("/register") ? "bg-gray-900 text-white" : "text-gray-300" ?> hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Register</a>
+                            <a href="/login"
+                                class="<?= urlIs("/login") ? "bg-gray-900 text-white" : "text-gray-300" ?> hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Login</a>
+
                             <?php endif; ?>
                         </button>
                     </div>
