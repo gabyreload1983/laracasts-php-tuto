acf2342b7b8e8689be56959ea522b6e097e32ae1
Gabriel Godoy <gabyreload@gmail.com>
2024-06-02 23:38:50 -0300

    Authenticator class

--- Http/controllers/session/store.php
+++ Http/controllers/session/store.php
@@ -1,39 +1,24 @@
 <?php
 
-use Core\App;
-use Core\Database;
-use Core\Validator;
+use Core\Authenticator;
 use Http\Forms\LoginForm;
 
-$db = App::resolve(Database::class);
 
 $email = $_POST["email"];
 $password = $_POST["password"];
 
 $form = new LoginForm;
 
-if(!$form->validate($email, $password)){
-    return view("session/create.view.php",[
-        "errors" => $form->errors(),
-    ]);
+if($form->validate($email, $password)){
+    $auth = new Authenticator();
+
+    if($auth->attempt($email, $password)){
+        redirect("/");
+    } 
+
+    $form->error( "password", "Email or password is wrong");
 }
-
-$user = $db->query("SELECT * FROM users WHERE email = :email",[
-    "email" => $email
-])->find();
-
-if($user && password_verify($password,$user["password"])){
-    login([
-        "email" => $email
-    ]);
-    
-    header("location: /");
-    exit();
-}
-
 
 return view("session/create.view.php",[
-    "errors" => [
-        "password" => "Email or password is wrong"
-    ],
+    "errors" => $form->errors()
 ]);
