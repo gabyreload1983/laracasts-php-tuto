7124f175d63e30c3937cba20045f4588453c012a
Gabriel Godoy <gabyreload@gmail.com>
2024-06-03 22:11:47 -0300

    PRG

--- Http/controllers/session/store.php
+++ Http/controllers/session/store.php
@@ -1,6 +1,7 @@
 <?php
 
 use Core\Authenticator;
+use Core\Session;
 use Http\Forms\LoginForm;
 
 
@@ -19,6 +20,6 @@ if($form->validate($email, $password)){
     $form->error( "password", "Email or password is wrong");
 }
 
-return view("session/create.view.php",[
-    "errors" => $form->errors()
-]);
+Session::flash("errors", $form->errors());
+
+return redirect("/login");
